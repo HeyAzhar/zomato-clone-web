48b14513f2a5a46389292f927bb69a9cf826b117
Sk Azharuddin <azhar@quoality.com>
2021-11-27 18:53:07 +0530

    Day 11

--- src/App/Login/index.js
+++ src/App/Login/index.js
@@ -1,8 +1,66 @@
+import { TextField, Button, Container, Grid } from "@mui/material";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
+  const [data, setData] = useState({});
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const URL = `${process.env.REACT_APP_API_URL}/user/login`;
+
+    const newUser = await axios
+      .post(URL, data)
+      .catch((error) => console.log("Something went wrong", error));
+
+    newUser?.data && navigate("/");
+
+    return;
+  };
+
   return (
-    <div>
+    <Container maxWidth='md'>
       <h1>Login page</h1>
-    </div>
+
+      <Grid item>
+        <TextField
+          defaultValue={data.phone || ""}
+          onChange={handleChange}
+          id='phone'
+          label='Phone'
+          variant='filled'
+        />
+      </Grid>
+
+      <Grid item>
+        <TextField
+          defaultValue={data.password || ""}
+          onChange={handleChange}
+          id='password'
+          type='password'
+          label='Password'
+          variant='filled'
+        />
+      </Grid>
+      <Grid item>
+        <Button onClick={handleLogin} variant='contained'>
+          Login
+        </Button>
+        <Link to='/signup'>
+          <Button variant='contained'>Signup</Button>
+        </Link>
+      </Grid>
+    </Container>
   );
 };
 
